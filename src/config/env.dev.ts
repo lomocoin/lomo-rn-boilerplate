85775699e74b15b78b5004134d4ee05c4ed6ed54
Tai Zhang <q6112345@gmail.com>
2018-10-12 11:13:48 +0800

    Check if is remote debug before setting BASE_URL

--- src/config/env.dev.ts
+++ src/config/env.dev.ts
@@ -1,10 +1,11 @@
 import { Platform } from 'react-native';
+const haveRemoteDev = (typeof DedicatedWorkerGlobalScope) !== 'undefined';
 
 // NOTE: on android to access the localhost we need to use 10.0.2.2
 
 export default {
   BASE_URL:
-    Platform.OS === 'android'
+    Platform.OS === 'android' && !haveRemoteDev
       ? 'http://10.0.2.2:3000'
       : 'http://localhost:3000',
   APP_NAME: 'LomoRN',
